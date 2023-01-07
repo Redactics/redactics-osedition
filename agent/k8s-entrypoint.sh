ac43a7d200c633143ebdcd83d451ca9b03ec0c72
Joe Auty <joe@datasprocket.com>
2023-01-06 22:25:01 -0800

    fixes for docker entrypoint workflow fetch

--- agent/k8s-entrypoint.sh
+++ agent/k8s-entrypoint.sh
@@ -13,18 +13,15 @@ cp /tmp/airflow-log-cleanup.py /opt/airflow/dags/
 cp /tmp/heartbeat.py /opt/airflow/dags/
 cp /tmp/sampletable-athletes.py /opt/airflow/dags/sampletable-athletes.py
 cp /tmp/sampletable-marketing_campaign.py /opt/airflow/dags/sampletable-marketing_campaign.py
-cp /tmp/erl-evaluation.py /opt/airflow/dags/erl-evaluation.py
 
 # fetch workflows
-workflows=`curl -s -H "Content-Type: application/json" ${API_URL}/workflow | jq -r .workflows`
+workflows=`curl -s -H "Content-Type: application/json" ${API_URL}/workflow | jq -r ".workflows"`
 total_workflows=`echo $workflows | jq length`
 
-#workflows_json=`echo $WORKFLOWS | base64 -d | jq .`
-#total_workflows=`echo $workflows_json | jq length`
 idx=0
 until [ $idx -eq $total_workflows ]; do
-  workflow_id=`echo $workflows | jq -r '.[$idx].uuid'`
-  workflow_type=`echo $workflows | jq -r '.[$idx].workflowType'`
+  workflow_id=`echo $workflows | jq -r ".[$idx].uuid"`
+  workflow_type=`echo $workflows | jq -r ".[$idx].workflowType"`
   if [ "$workflow_type" = "ERL" ]; then
     cp /tmp/redactics.py /opt/airflow/dags/${workflow_id}.py
   elif [ "$workflow_type" = "mockDatabaseMigration" ]; then
