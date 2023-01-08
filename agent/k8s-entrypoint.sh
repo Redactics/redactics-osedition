41dc997abeae1852fd05aea8541bcd14bb0f5218
Joe Auty <joe@datasprocket.com>
2023-01-07 17:46:00 -0800

    fetch workflows on schedule

--- agent/k8s-entrypoint.sh
+++ agent/k8s-entrypoint.sh
@@ -10,26 +10,12 @@ chmod 0600 /pgcerts/*/..data/sslkey || true
 echo "*** staging DAGs"
 cp /tmp/functions.sql /opt/airflow/dags/
 cp /tmp/airflow-log-cleanup.py /opt/airflow/dags/
+cp /tmp/refresh-dags.py /opt/airflow/dags/
 cp /tmp/heartbeat.py /opt/airflow/dags/
 cp /tmp/sampletable-athletes.py /opt/airflow/dags/sampletable-athletes.py
 cp /tmp/sampletable-marketing_campaign.py /opt/airflow/dags/sampletable-marketing_campaign.py
 
 # fetch workflows
-workflows=`curl -s -H "Content-Type: application/json" ${API_URL}/workflow | jq -r ".workflows"`
-total_workflows=`echo $workflows | jq length`
-
-idx=0
-until [ $idx -eq $total_workflows ]; do
-  workflow_id=`echo $workflows | jq -r ".[$idx].uuid"`
-  workflow_type=`echo $workflows | jq -r ".[$idx].workflowType"`
-  if [ "$workflow_type" = "ERL" ]; then
-    cp /tmp/redactics.py /opt/airflow/dags/${workflow_id}.py
-  elif [ "$workflow_type" = "mockDatabaseMigration" ]; then
-    cp /tmp/db-migration-mocking.py /opt/airflow/dags/${workflow_id}-migrationmocking.py
-  fi
-
-  let idx+=1
-done
-echo "*** DAGs staged"
+/fetch-workflows.sh
 
 exec "$@"
