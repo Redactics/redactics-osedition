006b59d8557d0f7a1927c1ee9bf67bc253af6f31
Joe Auty <joea@nulogy.com>
2023-10-25 21:29:38 -0700

    disable public schema creation hack

--- dockerimages/postgres-client/scripts/schema-dump.sh
+++ dockerimages/postgres-client/scripts/schema-dump.sh
@@ -8,10 +8,11 @@ SCHEMA=$2
 /scripts/prep-certs.sh
 
 echo "Creating SQL for schema $SCHEMA"
-if [ "${SCHEMA}" != "public" ]
-then
-    # copy public as well to restore assets that might not have been imported into custom schema
-    pg_dump --schema=public --schema=${SCHEMA} -s -O -x -v | curl -X POST -H "Transfer-Encoding: chunked" -s -T - http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}.sql
-else
-    pg_dump --schema=${SCHEMA} -s -O -x -v | curl -X POST -H "Transfer-Encoding: chunked" -s -T - http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}.sql
-fi
+# if [ "${SCHEMA}" != "public" ]
+# then
+#     # copy public as well to restore assets that might not have been imported into custom schema
+#     pg_dump --schema=public --schema=${SCHEMA} -s -O -x -v | curl -X POST -H "Transfer-Encoding: chunked" -s -T - http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}.sql
+# else
+#     pg_dump --schema=${SCHEMA} -s -O -x -v | curl -X POST -H "Transfer-Encoding: chunked" -s -T - http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}.sql
+# fi
+pg_dump --schema=${SCHEMA} -s -O -x -v | curl -X POST -H "Transfer-Encoding: chunked" -s -T - http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}.sql
