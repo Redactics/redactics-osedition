17e8ae9191e7f9873e89cd3df63de0212c3c7bff
Joe Auty <joe@datasprocket.com>
2024-02-15 20:58:49 -0800

    sequence fix

--- dockerimages/postgres-client/scripts/generate-sequences-sql.sh
+++ dockerimages/postgres-client/scripts/generate-sequences-sql.sh
@@ -10,11 +10,28 @@ SEQEUNCE_CMDS=$3
 
 PSQL="psql --quiet --tuples-only --no-align"
 
-# generate DDL to restore sequences which needs to be done in advance of remaining DDL
-pg_dump --schema=${SCHEMA} -s -O -x | awk '/[CREATE|ALTER] SEQUENCE/ {print}' FS="\n" RS="" | curl -X POST -H "Transfer-Encoding: chunked" -s -T - http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}-sequences.sql
-
 # Nulogy patch for setting sequences
 if [ -z "$SEQUENCE_CMDS" ]
 then
     echo $SEQEUNCE_CMDS | curl -X POST -H "Transfer-Encoding: chunked" -s -f -T - http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}-set-sequences.sql
+else
+    $PSQL -c "SELECT 
+    'SELECT SETVAL(' ||
+       quote_literal(quote_ident(sequence_namespace.nspname) || '.' || quote_ident(class_sequence.relname)) ||
+       ', COALESCE(MAX(' ||quote_ident(pg_attribute.attname)|| '), 1) ) FROM ' ||
+       quote_ident(table_namespace.nspname)|| '.'||quote_ident(class_table.relname)|| ';'
+FROM pg_depend 
+    INNER JOIN pg_class AS class_sequence
+        ON class_sequence.oid = pg_depend.objid 
+            AND class_sequence.relkind = 'S'
+    INNER JOIN pg_class AS class_table
+        ON class_table.oid = pg_depend.refobjid
+    INNER JOIN pg_attribute 
+        ON pg_attribute.attrelid = class_table.oid
+            AND pg_depend.refobjsubid = pg_attribute.attnum
+    INNER JOIN pg_namespace as table_namespace
+        ON table_namespace.oid = class_table.relnamespace AND table_namespace.nspname = '${SCHEMA}'
+    INNER JOIN pg_namespace AS sequence_namespace
+        ON sequence_namespace.oid = class_sequence.relnamespace
+ORDER BY sequence_namespace.nspname, class_sequence.relname;" | curl -X POST -H "Transfer-Encoding: chunked" -s -f -T - http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}-set-sequences.sql
 fi
--- dockerimages/postgres-client/scripts/schema-restore.sh
+++ dockerimages/postgres-client/scripts/schema-restore.sh
@@ -10,8 +10,4 @@ SCHEMA=$3
 
 echo "Restoring SQL for schema $SCHEMA"
 
-# restore sequences first
-curl -fs http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}-sequences.sql | psql -d ${TARGET_DATABASE}
-
-# restore remaining DDL
 curl -fs http://agent-http-nas:3000/file/${WORKFLOW}%2Fschema-${SCHEMA}.sql | psql -d ${TARGET_DATABASE}
